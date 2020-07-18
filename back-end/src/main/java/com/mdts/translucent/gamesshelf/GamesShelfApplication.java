5c524ef0e312a8d81fa6fe14ae0e127d1a8c20af
Mailson Dennis <mailssondennis@gmail.com>
2020-07-18 14:51:21 -0300

    config: back-end initial structure

---
+++ back-end/src/main/java/com/mdts/translucent/gamesshelf/GamesShelfApplication.java
@@ -0,0 +1,13 @@
+package com.mdts.translucent.gamesshelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesShelfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamesShelfApplication.class, args);
+    }
+
+}
---
+++ back-end/src/test/java/com/mdts/translucent/gamesshelf/GamesShelfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdts.translucent.gamesshelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesShelfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
