09118dd1cd5efb3dc606b510edada09632519b5b
Mailson Dennis <mailssondennis@gmail.com>
2020-07-19 15:56:57 -0300

    fix: add cross origin config in back-end

--- back-end/src/main/java/com/mdts/translucent/gamesshelf/controller/GameController.java
+++ back-end/src/main/java/com/mdts/translucent/gamesshelf/controller/GameController.java
@@ -15,6 +15,7 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @RequestMapping("games")
+@CrossOrigin(origins = "http://localhost:4200")
 public class GameController {
 
     private final GameService gameService;
